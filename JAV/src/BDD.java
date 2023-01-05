62517e46ebd7cf339bd87b9ef8149c08f3255172
ben1frt <benjamin.faretie@etu.unice.fr>
2023-01-05 15:40:08 +0100

    sss

--- JAV/src/BDD.java
+++ JAV/src/BDD.java
@@ -1,11 +1,15 @@
-import java.sql.Statement;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
-public class App {
+public class SAE302_main {
 
     private Connection connexion;
 
@@ -13,34 +17,19 @@ public class App {
         return connexion;
     }
 
-    public void searchIntoDb () {
-        String sql;
-        ResultSet rs;
-
-        sql= "select * from Infos";
-        rs= null;
-
-        try (
-             PreparedStatement pstmt  = connexion.prepareStatement(sql)){
-            rs  = pstmt.executeQuery();
-
-            // Traitement du résultat
-            while (rs.next()) {
-                System.out.println(
-                    rs.getInt("id") +  "\t" + 
-                    rs.getString("nom") + "\t" +
-                    rs.getInt("annee")
-                );
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }  
+    //télécharger un fichier .db depuis une url
+    public static void downloadFile(String url, String fileName) throws IOException {
+        URL website = new URL(url);
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream(fileName);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
     }
 
-    public App () {
+    public SAE302_main () {
         try {
-            // L'url d'accès ici juste en dessous faut mettre un .db, je n'en ai pas trouvé
-            String url = "jdbc:sqlite:./demo.db";
+            // L'url d'accès
+            String url = "jdbc:sqlite:./ecowattest.db";
             // Créer une bd ou l'ouvrir si existante
             connexion = DriverManager.getConnection(url);
             System.out.println("Connexion à SQLite établie.");    
@@ -49,11 +38,38 @@ public class App {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        App p;
+    public void searchIntoDb () {
+        String sql;
+        ResultSet rs;
 
-        p= new App();
-        /*
-        p.searchIntoDb();*/
+        sql = "select * from Infos";
+        rs = null;
+
+        try (
+             PreparedStatement pstmt  = connexion.prepareStatement(sql)){
+            rs = pstmt.executeQuery();
+
+            // Traitement du résultat
+            while (rs.next()) {
+                System.out.println(
+                    rs.getInt("id") +  "\t" + 
+                    rs.getString("data")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    //main
+    public static void main(String[] args) throws Exception {
+        //télécharger le fichier .db
+        downloadFile("http://isis.unice.fr/~mgautero/ext/sae302/bd/ecowatt.db", "ecowattest.db");
+        //afficher le contenu de la bd
+        SAE302_main app = new SAE302_main();
+        app.searchIntoDb();
+
+    }
+
+    
 }
