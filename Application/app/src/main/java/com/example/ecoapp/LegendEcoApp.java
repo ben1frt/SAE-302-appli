52d65d28e90ade1ffe926bf220a91e86373b2d72
ben1frt <benjamin.faretie@etu.unice.fr>
2023-01-16 21:57:03 +0100

    app

---
+++ Application/app/src/main/java/com/example/ecoapp/LegendEcoApp.java
@@ -0,0 +1,14 @@
+package com.example.ecoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LegendEcoApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legend_eco_app);
+    }
+}
--- Application/app/src/main/java/com/example/ecoapp/MainActivity.java
+++ Application/app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.ecoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    // Méthode DisplayLegend
+    public void DisplayLegend (View view) {
+        Intent intent = new Intent(this, LegendEcoApp.class);
+        startActivity(intent);
     }
 }
