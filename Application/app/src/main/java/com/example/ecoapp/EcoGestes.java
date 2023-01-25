151c4bcef15b402638d4ceda9d31e580ad4833bc
ben1frt <benjamin.faretie@etu.unice.fr>
2023-01-25 17:18:19 +0100

    cc

---
+++ Application/app/src/main/java/com/example/ecoapp/EcoGestes.java
@@ -0,0 +1,14 @@
+package com.example.ecoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EcoGestes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eco_gestes);
+    }
+}
---
+++ Application/app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ecoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+
+        //Button ecobutton = findViewById(R.id.EcoButton);
+        //AnimationDrawable animationDrawable1 = (AnimationDrawable) ecobutton.getBackground();
+        //animationDrawable1.setEnterFadeDuration(500);
+        //animationDrawable1.setExitFadeDuration(1000);
+        //animationDrawable1.start();
+    }
+
+    // Méthode DisplayLegend
+    public void DisplayEcoGestes (View view) {
+        Intent intent = new Intent(this, EcoGestes.class);
+        startActivity(intent);
+    }
+}
